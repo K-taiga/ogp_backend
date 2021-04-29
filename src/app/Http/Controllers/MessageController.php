9e241208e188f0d425c7e8856438cc128ab3dfec
K-taiga <taigakatou0329@gmail.com>
2021-04-30 08:01:10 +0900

    php fmt

--- src/app/Http/Controllers/MessageController.php
+++ src/app/Http/Controllers/MessageController.php
@@ -11,18 +11,18 @@ use Str;
 
 class MessageController extends Controller {
     public function store(Request $request) {
-        $params  = $request->json()->all();
+        $params = $request->json()->all();
 
         // list(,)で配列の最初を飛ばし';'以降を取得
-        list(, $image) = explode(';',$params['image']);
+        list(, $image) = explode(';', $params['image']);
         // list(,)で配列の最初を飛ばし','以降を取得
-        list(, $image) = explode(',',$image);
-        $decodedImage = base64_decode($image);
+        list(, $image) = explode(',', $image);
+        $decodedImage  = base64_decode($image);
 
         $content = $params['message'];
         // callback関数
-        $id      = DB::transaction(function() use ($decodedImage,$content) {
-            $id      = Str::uuid();
+        $id = DB::transaction(function () use ($decodedImage, $content) {
+            $id   = Str::uuid();
             $file = $id->toString() . '.jpg';
             Message::create([
                 'id'        => $id,
@@ -30,16 +30,15 @@ class MessageController extends Controller {
                 'file_path' => $file,
             ]);
 
-            $isSuccess = Storage::disk('s3')->put($file,$decodedImage);
-            if(!$isSuccess) {
+            $isSuccess = Storage::disk('s3')->put($file, $decodedImage);
+            if (!$isSuccess) {
                 throw new Exception('ファイルアップロード時にエラーが発生しました。');
             }
             // 外部からアクセス可能にする
-            Storage::disk('s3')->setVisibility($file,public);
+            Storage::disk('s3')->setVisibility($file, 'public');
 
             return $id;
         });
-
 
         return response()->json($id);
     }
